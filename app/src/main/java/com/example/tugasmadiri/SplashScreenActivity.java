c900e11d7caa5af0e5d0cb866299bfea17bc8599
milyardinardin <labanamilyardin@gmail.com>
2021-05-05 21:23:34 +0700

    tugas mandiri

---
+++ app/src/main/java/com/example/tugasmadiri/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.example.tugasmadiri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private int waktu_loading=3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home=new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },waktu_loading);
+    }
+}
